6ef6d3f1a5dde13bdd6ce8481d3a8beb670efa38
oksbelysheva <oks.belysheva@yandex.ru>
2019-09-23 18:32:44 +0400

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = [];
+  bracketsConfig.forEach(element => {
+    if (Number.isInteger(+element[0]))
+      brackets.push(element[0] + '' + element[1]);
+    else
+      brackets.push('\\' + element[0] + '\\' + element[1]);
+  });
+
+  let oldLength;
+  do {
+    oldLength = str.length;
+    brackets.forEach(element => {
+      str = str.replace(new RegExp(element, 'g'),'');
+    })
+  } while(str.length && oldLength != str.length)
+  
+  return str.length == 0;
 }
